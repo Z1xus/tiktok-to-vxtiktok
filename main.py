f82838fdaec47e8e3fa423052c2489d0cbcf7a7b
Z1xus <40185941+Z1xus@users.noreply.github.com>
2023-12-10 17:25:54 +0100

    handle message can't be deleted exception, log timestamps

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import asyncio
 from aiogram import Bot, Dispatcher, Router, types
 from aiogram.filters import Command
 from aiogram.types import InlineQueryResultArticle, InputTextMessageContent
+from aiogram.exceptions import TelegramForbiddenError, TelegramBadRequest
 from aiohttp import ClientSession
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
@@ -16,7 +17,11 @@ load_dotenv()
 TOKEN = getenv("TOKEN") or exit("TOKEN is not set in the .env!")
 MONGODB_URI = getenv("MONGODB_URI") or exit("MONGODB_URI is not set in the .env!")
 
-logging.basicConfig(level=logging.WARNING)
+logging.basicConfig(
+    level=logging.WARNING,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 router = Router()
 client = AsyncIOMotorClient(MONGODB_URI)
@@ -32,16 +37,21 @@ async def get_redirected_url(url: str) -> str:
 
 @router.message(Command(commands=["start"]))
 async def command_start(message: types.Message, bot: Bot) -> None:
-    await bot.send_message(message.chat.id, "send me a tiktok link and I'll convert it to a vxtiktok link :)")
+    await bot.send_message(
+        message.chat.id,
+        "send me a tiktok link and I'll convert it to a vxtiktok link :)",
+    )
 
 
 @router.message(Command(commands=["about"]))
 async def command_about(message: types.Message, bot: Bot) -> None:
     await bot.send_message(
         message.chat.id,
-        'this bot is open-source and avalible on <a href="https://github.com/z1xus/tiktok-to-vxtiktok">github</a>!\n'
+        "this bot is open-source and avalible on "
+        '<a href="https://github.com/z1xus/tiktok-to-vxtiktok">github</a>!\n'
         '\nwhat is <a href="https://github.com/dylanpdx/vxtiktok">vxtiktok</a>?\n'
-        "it's a web service that fixes tiktok links embedding on various platforms by enabling the ability to watch the video directly in the embed rather than having to open the app.\n"
+        "it's a web service that fixes tiktok links embedding on various platforms by "
+        "enabling the ability to watch the video directly in the embed rather than having to open the app.\n"
         '\ncontact me if you have questions <a href="https://t.me/z1xus">@z1xus</a> &lt;3',
         parse_mode="HTML",
         disable_web_page_preview=True,
@@ -51,7 +61,9 @@ async def command_about(message: types.Message, bot: Bot) -> None:
 @router.message(Command(commands=["replace"]))
 async def command_toggle(message: types.Message, bot: Bot) -> None:
     if message.chat.type == "private":
-        await bot.send_message(message.chat.id, "this command is intended to be used in group chats ❌")
+        await bot.send_message(
+            message.chat.id, "this command is intended to be used in group chats ❌"
+        )
         return
 
     chat_data = await chats_collection.find_one({"chat_id": message.chat.id}) or {}
@@ -63,39 +75,69 @@ async def command_toggle(message: types.Message, bot: Bot) -> None:
         {"$set": {"replace_toggle_state": new_state}},
         upsert=True,
     )
-    await bot.send_message(
-        message.chat.id,
+    await message.reply(
         f"message replacing is {'enabled ✅' if new_state else 'disabled ❌'}",
     )
 
 
 @router.message()
 async def convert_link(message: types.Message, bot: Bot) -> None:
-    tiktok_link = re.findall(r"(https?://(?:www\.|vm\.)?tiktok\.com/[^\s]+)", message.text)
-    if tiktok_link:
-        try:
-            redirected_url = (
-                tiktok_link[0]
-                if "www.tiktok.com" in tiktok_link[0]
-                else await get_redirected_url(tiktok_link[0])
+    tiktok_link = re.findall(
+        r"(https?://(?:www\.|vm\.)?tiktok\.com/[^\s]+)", message.text
+    )
+    if not tiktok_link:
+        if message.chat.type == "private" and re.findall(
+            r"(https?://[^\s]+)", message.text
+        ):
+            await bot.send_message(
+                message.chat.id, "this doesn't look like a tiktok link to me 🤨"
             )
-            vxtiktok_link = redirected_url.split("?")[0].replace("www.tiktok.com", "vxtiktok.com")
+        return
 
-            if message.chat.type != "private":
-                chat_data = await chats_collection.find_one({"chat_id": message.chat.id}) or {}
-                if chat_data.get("replace_toggle_state", False):
+    try:
+        redirected_url = (
+            tiktok_link[0]
+            if "www.tiktok.com" in tiktok_link[0]
+            else await get_redirected_url(tiktok_link[0])
+        )
+        vxtiktok_link = redirected_url.split("?")[0].replace(
+            "www.tiktok.com", "vxtiktok.com"
+        )
+
+        if message.chat.type != "private":
+            chat_data = (
+                await chats_collection.find_one({"chat_id": message.chat.id}) or {}
+            )
+            if chat_data.get("replace_toggle_state", False):
+                try:
                     await bot.delete_message(message.chat.id, message.message_id)
-            await bot.send_message(message.chat.id, vxtiktok_link)
-        except Exception as e:
-            logging.error(e)
-            await bot.send_message(message.chat.id, "oops, I failed to convert your tiktok link\ncontact the dev @z1xus")
-    elif message.chat.type == "private" and re.findall(r"(https?://[^\s]+)", message.text):
-        await bot.send_message(message.chat.id, "this doesn't look like a tiktok link to me 🤨")
+                except TelegramBadRequest:
+                    await bot.send_message(
+                        message.chat.id,
+                        "sorry, i have no permission to delete messages! ;'(\n"
+                        "please consider disabling replacing (/replace) or"
+                        "promoting the bot to admin",
+                    )
+                except TelegramNotFound as e:
+                    logging.warning(
+                        f"Failed to delete message: {e} - chat_id={message.chat.id}, message_id={message.message_id}"
+                    )
+                    pass
+
+        await bot.send_message(message.chat.id, vxtiktok_link)
+    except Exception as e:
+        logging.error(e)
+        await bot.send_message(
+            message.chat.id,
+            "oops, I failed to convert your tiktok link" "\ncontact the dev @z1xus",
+        )
 
 
 @router.inline_query()
 async def inline_query_handler(inline_query: types.InlineQuery, bot: Bot) -> None:
-    tiktok_link = re.findall(r"(https?://(?:www\.|vm\.)?tiktok\.com/[^\s]+)", inline_query.query)
+    tiktok_link = re.findall(
+        r"(https?://(?:www\.|vm\.)?tiktok\.com/[^\s]+)", inline_query.query
+    )
     if tiktok_link:
         try:
             redirected_url = (
@@ -103,15 +145,21 @@ async def inline_query_handler(inline_query: types.InlineQuery, bot: Bot) -> Non
                 if "www.tiktok.com" in tiktok_link[0]
                 else await get_redirected_url(tiktok_link[0])
             )
-            vxtiktok_link = redirected_url.split("?")[0].replace("www.tiktok.com", "vxtiktok.com")
+            vxtiktok_link = redirected_url.split("?")[0].replace(
+                "www.tiktok.com", "vxtiktok.com"
+            )
             item = InlineQueryResultArticle(
                 id="tiktok_link",
                 title="converted link",
-                input_message_content=InputTextMessageContent(message_text=vxtiktok_link),
+                input_message_content=InputTextMessageContent(
+                    message_text=vxtiktok_link
+                ),
                 description=vxtiktok_link,
                 thumbnail_url="https://github.com/Z1xus/tiktok-to-vxtiktok/blob/main/assets/cooltent.webp",
             )
-            await bot.answer_inline_query(inline_query.id, results=[item], cache_time=30)
+            await bot.answer_inline_query(
+                inline_query.id, results=[item], cache_time=30
+            )
         except Exception as e:
             logging.error(e)
             await bot.answer_inline_query(inline_query.id, results=[], cache_time=30)
@@ -119,7 +167,9 @@ async def inline_query_handler(inline_query: types.InlineQuery, bot: Bot) -> Non
         item = InlineQueryResultArticle(
             id="not_valid",
             title="not a valid link",
-            input_message_content=InputTextMessageContent(message_text="❌ no valid link provided"),
+            input_message_content=InputTextMessageContent(
+                message_text="❌ no valid link provided"
+            ),
             description="this doesn't look like a tiktok link to me 🤨",
             thumbnail_url="https://github.com/Z1xus/tiktok-to-vxtiktok/blob/main/assets/shrugtent.webp",
         )
